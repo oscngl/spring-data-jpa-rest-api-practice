b0dac1897f1d26fdf4fa35e239a1e260e1e896e6
Ömer Said ÇENGEL <oscngl@gmail.com>
2022-02-08 16:59:48 +0300

    Initial commit

---
+++ src/main/java/com/osc/spring_data_jpa_rest_api_practice/Application.java
@@ -0,0 +1,13 @@
+package com.osc.spring_data_jpa_rest_api_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/osc/spring_data_jpa_rest_api_practice/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osc.spring_data_jpa_rest_api_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
