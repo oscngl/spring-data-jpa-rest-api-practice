9c7701d5f2439ce0d4a3efa89b22dcdb207ffa8a
Ömer Said ÇENGEL <oscngl@gmail.com>
2022-02-08 17:05:09 +0300

    Entity created.

---
+++ src/main/java/com/osc/spring_data_jpa_rest_api_practice/entities/concretes/Student.java
@@ -0,0 +1,27 @@
+package com.osc.spring_data_jpa_rest_api_practice.entities.concretes;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "students")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+}
